9b0bc2f3eb3c72416b6c548f52d291a1b4ab95ad
Tomek Meka <tomek@gildia.pl>
2012-04-05 09:43:49 +0200

    Regenerate gemspec for version 0.0.10

--- lib/elibri_onix_dict/version.rb
+++ lib/elibri_onix_dict/version.rb
@@ -5,7 +5,7 @@ module Elibri
       module Version
         MAJOR = 0
         MINOR = 0
-        PATCH = 9
+        PATCH = 10
 
         STRING = [MAJOR, MINOR, PATCH].compact.join('.')
       end
