d82f4220a16ad2e36321ed842657e67bac9c039f
Marcin Urbański <marcin@urbanski.vdl.pl>
2012-04-25 12:05:44 +0200

    E-pub usage units dictionary

--- lib/elibri_onix_dict/version.rb
+++ lib/elibri_onix_dict/version.rb
@@ -5,7 +5,7 @@ module Elibri
       module Version
         MAJOR = 0
         MINOR = 0
-        PATCH = 11
+        PATCH = 12
 
         STRING = [MAJOR, MINOR, PATCH].compact.join('.')
       end
--- test/elibri_onix_dict_release_3_0_test.rb
+++ test/elibri_onix_dict_release_3_0_test.rb
@@ -12,6 +12,7 @@ describe Elibri::ONIX::Dict::Release_3_0 do
     assert_equal 'BA', Elibri::ONIX::Dict::Release_3_0::ProductFormCode::BOOK
     assert_equal '02', Elibri::ONIX::Dict::Release_3_0::EpubUsageStatus::LIMITED
     assert_equal '07', Elibri::ONIX::Dict::Release_3_0::EpubUsageType::TIME_LIMITED
+    assert_equal '09', Elibri::ONIX::Dict::Release_3_0::EpubUsageUnit::DAYS
 
     form = Elibri::ONIX::Dict::Release_3_0::ProductFormCode.find_by_onix_code('BA')
     assert_equal 'książka', form.name 
