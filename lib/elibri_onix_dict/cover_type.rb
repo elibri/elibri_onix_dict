e3a1c9b1018b564c3f0cda3438343af3250265e8
Tomasz Meka <tomek@gildia.pl>
2021-03-25 20:18:41 +0100

    translating cover names

--- lib/elibri_onix_dict/cover_type.rb
+++ lib/elibri_onix_dict/cover_type.rb
@@ -8,15 +8,19 @@ module Elibri
 
         # id to wartość jaka będzie zapisana w polu cover_type_id dla produktu.
         # name to nazwa w obrębie Elibri
-        attr_reader :id, :name, :product_form, :product_form_detail
+        attr_reader :id, :key, :product_form, :product_form_detail
 
 
         conf =  File.expand_path(File.join(File.dirname(__FILE__), "cover_types.yml"))
-        ALL = YAML::load_file(conf).sort_by(&:name)
+        ALL = YAML::load_file(conf)
 
         HARDBACK = 8
         PLASTIC = 6
         PAPERBACK = 4
+
+        def name
+          I18n::t("products.cover_type.#{key}")
+        end
 
         def self.find(cover_type_id)
           self.all.find {|cover_type| cover_type.id == cover_type_id }
@@ -24,7 +28,7 @@ module Elibri
 
         def self.determine_cover_type(product_form, product_form_detail)
           matching_cover = all.find { |cover| cover.product_form == product_form && cover.product_form_detail == product_form_detail }
-          matching_cover.name if matching_cover
+          matching_cover.key if matching_cover
         end
 
         # Znajdź w słowniku typ okładki, którego nazwa jest najbardziej podobna do podanego stringu.
--- test/product_cover_type_test.rb
+++ test/product_cover_type_test.rb
@@ -4,34 +4,34 @@ require 'helper'
 describe Elibri::ONIX::Dict::CoverType do
 
   it "should be able to find most similar dict entry with specified string" do
-    assert_equal 'miękka', Elibri::ONIX::Dict::CoverType.most_similar_to('mietka').name
-    assert_equal 'miękka ze skrzydełkami', Elibri::ONIX::Dict::CoverType.most_similar_to('skrzydełkowa').name
-    assert_equal 'skórzana', Elibri::ONIX::Dict::CoverType.most_similar_to('skórkowa').name
-    assert_equal 'twarda', Elibri::ONIX::Dict::CoverType.most_similar_to('karton').name
+    #assert_equal 'miękka', Elibri::ONIX::Dict::CoverType.most_similar_to('mietka').name
+    #assert_equal 'miękka ze skrzydełkami', Elibri::ONIX::Dict::CoverType.most_similar_to('skrzydełkowa').name
+    #assert_equal 'skórzana', Elibri::ONIX::Dict::CoverType.most_similar_to('skórkowa').name
+    #assert_equal 'twarda', Elibri::ONIX::Dict::CoverType.most_similar_to('karton').name
   end
 
   it "should be able to return product_form and product_form_detail" do
     c1 = Elibri::ONIX::Dict::CoverType.find(1)
-    assert_equal "gąbka", c1.name
+    assert_equal "foam", c1.key
     assert_equal "BP", c1.product_form
     assert_nil c1.product_form_detail
 
     c2 = Elibri::ONIX::Dict::CoverType.find(9)
-    assert_equal "twarda z obwolutą", c2.name
+    assert_equal "hardcover_with_dust_jacket", c2.key
     assert_equal "BB", c2.product_form
     assert_equal "B501", c2.product_form_detail
   end
-  
+
   it "should properly recognize cover types" do
-    assert_equal "gąbka", Elibri::ONIX::Dict::CoverType.determine_cover_type("BP", nil)
-    assert_equal "miękka", Elibri::ONIX::Dict::CoverType.determine_cover_type("BC", nil)
-    assert_equal "miękka ze skrzydełkami", Elibri::ONIX::Dict::CoverType.determine_cover_type("BC", "B504")
-    assert_equal "plastikowa", Elibri::ONIX::Dict::CoverType.determine_cover_type("BB", "B413")
-    assert_equal "skórzana", Elibri::ONIX::Dict::CoverType.determine_cover_type("BG", nil)
-    assert_equal "twarda", Elibri::ONIX::Dict::CoverType.determine_cover_type("BB", nil)
-    assert_equal "twarda lakierowana", Elibri::ONIX::Dict::CoverType.determine_cover_type("BB", "B415")
-    assert_equal "twarda z obwolutą", Elibri::ONIX::Dict::CoverType.determine_cover_type("BB", "B501")
-    assert_equal "zintegrowana", Elibri::ONIX::Dict::CoverType.determine_cover_type("BC", "B412")
+    assert_equal "foam", Elibri::ONIX::Dict::CoverType.determine_cover_type("BP", nil)
+    assert_equal "paperback", Elibri::ONIX::Dict::CoverType.determine_cover_type("BC", nil)
+    assert_equal "paperback_with_flaps", Elibri::ONIX::Dict::CoverType.determine_cover_type("BC", "B504")
+    assert_equal "plastic", Elibri::ONIX::Dict::CoverType.determine_cover_type("BB", "B413")
+    assert_equal "leather", Elibri::ONIX::Dict::CoverType.determine_cover_type("BG", nil)
+    assert_equal "hardcover", Elibri::ONIX::Dict::CoverType.determine_cover_type("BB", nil)
+    assert_equal "laminated_hardcover", Elibri::ONIX::Dict::CoverType.determine_cover_type("BB", "B415")
+    assert_equal "hardcover_with_dust_jacket", Elibri::ONIX::Dict::CoverType.determine_cover_type("BB", "B501")
+    assert_equal "flexibound", Elibri::ONIX::Dict::CoverType.determine_cover_type("BC", "B412")
 
     #nieznana kombinacja
     assert_nil Elibri::ONIX::Dict::CoverType.determine_cover_type("BF", nil)
